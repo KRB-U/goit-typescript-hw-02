a3710f61bac2eb696c98761f16e26a3339a8b45b
unknown <rubikonspace@ukr.net>
2023-11-24 18:25:42 +0200

    5

--- index.js
+++ index.js
@@ -246,4 +246,62 @@ console.log(RoleDescription);
 //   ram: 16,
 //   storage: 512,
 // };
+// *************
+// type AdminType = {
+//   type: "admin";
+//   name: string;
+//   privileges: string[];
+// };
+// type UserType = {
+//   type: "user";
+//   name: string;
+//   registrationDate: Date;
+// };
+// function checkUser(name: string, type: "user"): UserType;
+// function checkUser(
+//   name: string,
+//   type: "admin",
+//   privileges: string[]
+// ): AdminType;
+// function checkUser(
+//   name: string,
+//   type: "admin" | "user",
+//   privilegesOrRegistrationDate?: string[]
+// ): AdminType | UserType {
+//   if (type === "admin") {
+//     return {
+//       type,
+//       name,
+//       privileges: privilegesOrRegistrationDate as string[],
+//     };
+//   }
+//   return {
+//     type,
+//     name,
+//     registrationDate: new Date(),
+//   };
+// }
+// const user = checkUser("Nikita", "user");
+// const admin = checkUser("Tonya", "admin", ["manageUsers", "deletePosts"]);
+// // console.log(admin);
+// **********
+// let arr6: Array<string | number> = [];
+// arr6 = ["sgdshmj", "dgfdfg", 345];
+// **********
+// const promise: Promise<string> = new Promise((resolve) => {
+//   setInterval(() => {
+//     resolve("Done!");
+//   }, 1000);
+// });
+// promise.then((data) => {
+//   console.log(data);
+// });
+// ****************
+// function dataView<T>(arg: T): T {
+//   return arg;
+// }
+// let result = dataView<string>("gsfsvbg");
+// console.log(result);
+// let result2 = dataView(556);
+// console.log(typeof result2);
 //# sourceMappingURL=index.js.map
